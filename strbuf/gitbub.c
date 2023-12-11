a58cd8061f6178f5f595da0f89d83dee798d0f5d
tiny-sky <118060227+tiny-sky@users.noreply.github.com>
2023-12-11 21:04:29 +0800

    Update gitbub.c

--- strbuf/gitbub.c
+++ strbuf/gitbub.c
@@ -1,10 +1,10 @@
 //#include "strbuf.h"
 
-// struct strbuf {
-//   int len;     //当前缓冲区（字符串）长度
-//   int alloc;   //当前缓冲区（字符串）容量
-//   char *buf;   //缓冲区（字符串）
-// };
+struct strbuf {
+  int len;     //当前缓冲区（字符串）长度
+  int alloc;   //当前缓冲区（字符串）容量
+  char *buf;   //缓冲区（字符串）
+};
 
 void strbuf_init(struct strbuf *sb, size_t alloc)
 {
